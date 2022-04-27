7ca2fdc90fe19956fb4950379a116e22ae60e71b
Daniela <dannytorres0211@gmail.com>
2022-04-27 11:26:16 -0500

    Agregar ejercicio Node.js Daniela Torres - Valentina Paredes

---
+++ name.js
@@ -0,0 +1,20 @@
+const { faker } = require('@faker-js/faker');
+const fs = require("file-system");
+
+let arrayNames = [];
+
+for(let i=1; i <= 1000; i++){
+    const randomName = faker.name.findName();
+    arrayNames.push(randomName);   
+}
+
+const names = arrayNames.join(", ");
+fs.writeFile("names.txt", names, (err) => {
+    if (err)
+      console.log(err);
+    else {
+      console.log("File written successfully\n");
+      console.log("The written has the following contents:");
+      console.log(fs.readFileSync("names.txt", "utf8"));
+    }
+  });
